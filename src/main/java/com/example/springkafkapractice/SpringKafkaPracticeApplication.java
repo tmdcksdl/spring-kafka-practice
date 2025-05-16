facfbc56018cf7cc3da17f06a8beb5d06d41fcac
tmdcksdl <tmdcksdl99@naver.com>
2025-05-16 16:17:34 +0900

    📝 chore: 기본 세팅

---
+++ src/main/java/com/example/springkafkapractice/SpringKafkaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springkafkapractice/SpringKafkaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
